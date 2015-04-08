355c04e2c4905baafa74e74f9e1970a62e586e41
watemeion <g.yubao@gmail.com>
2015-04-08 20:14:22 +0800

    Initial Commit

---
+++ ejianzhi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ejianzhi
+//
+//  Created by Mac on 4/8/15.
+//  Copyright (c) 2015 Studio Of Spicy Hot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ejianzhi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ejianzhi
+//
+//  Created by Mac on 4/8/15.
+//  Copyright (c) 2015 Studio Of Spicy Hot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
