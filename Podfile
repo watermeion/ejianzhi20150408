0bfdd4de7798c3d83f609d39d04f54a1ddcba889
watemeion <g.yubao@gmail.com>
2015-04-09 11:31:13 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+pod 'ReactiveCocoa'
+pod 'UIAlertView-Blocks'
+pod 'AVOSCloud'
+pod 'AMap2DMap' #2D地图SDK(2D地图和3D地图不能同时使用)
+pod 'AMapSearch' #搜索服务SDK
+
+target 'ejianzhi' do
+
+end
+
+target 'ejianzhiTests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,56 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AMap2DMap
+#define COCOAPODS_POD_AVAILABLE_AMap2DMap
+#define COCOAPODS_VERSION_MAJOR_AMap2DMap 2
+#define COCOAPODS_VERSION_MINOR_AMap2DMap 4
+#define COCOAPODS_VERSION_PATCH_AMap2DMap 2
+
+// AMapSearch
+#define COCOAPODS_POD_AVAILABLE_AMapSearch
+#define COCOAPODS_VERSION_MAJOR_AMapSearch 2
+#define COCOAPODS_VERSION_MINOR_AMapSearch 4
+#define COCOAPODS_VERSION_PATCH_AMapSearch 1
+
+// AVOSCloud
+#define COCOAPODS_POD_AVAILABLE_AVOSCloud
+#define COCOAPODS_VERSION_MAJOR_AVOSCloud 3
+#define COCOAPODS_VERSION_MINOR_AVOSCloud 0
+#define COCOAPODS_VERSION_PATCH_AVOSCloud 2
+
+// ReactiveCocoa
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 7
+
+// ReactiveCocoa/Core
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 7
+
+// ReactiveCocoa/UI
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_UI
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_UI 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_UI 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_UI 7
+
+// ReactiveCocoa/no-arc
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 7
+
+// UIAlertView-Blocks
+#define COCOAPODS_POD_AVAILABLE_UIAlertView_Blocks
+#define COCOAPODS_VERSION_MAJOR_UIAlertView_Blocks 1
+#define COCOAPODS_VERSION_MINOR_UIAlertView_Blocks 0
+#define COCOAPODS_VERSION_PATCH_UIAlertView_Blocks 0
+
