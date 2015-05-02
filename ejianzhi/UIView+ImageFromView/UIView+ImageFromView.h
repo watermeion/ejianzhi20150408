f1a240b5943da2cbe6091c2b4cc3e2ac52a7b2c8
watemeion <g.yubao@gmail.com>
2015-05-02 09:20:15 +0800

    fix bug 20150501 by gyb

---
+++ ejianzhi/UIView+ImageFromView/UIView+ImageFromView.h
@@ -0,0 +1,13 @@
+//
+//  UIView+ImageFromView.h
+//  ejianzhi
+//
+//  Created by Mac on 4/24/15.
+//  Copyright (c) 2015 Studio Of Spicy Hot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ImageFromView)
+
+@end
