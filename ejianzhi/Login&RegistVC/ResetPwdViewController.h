c0e1f51daaa97e25d381c040ce69c6aab2994d27
watemeion <g.yubao@gmail.com>
2015-05-17 09:07:18 +0800

    20150518

---
+++ ejianzhi/Login&RegistVC/ResetPwdViewController.h
@@ -0,0 +1,17 @@
+//
+//  ResetPwdViewController.h
+//  ejianzhi
+//
+//  Created by Mac on 5/11/15.
+//  Copyright (c) 2015 Studio Of Spicy Hot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResetPwdViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *phoneText;
+
+@property (weak, nonatomic) IBOutlet UITextField *pwdText;
+@property (weak, nonatomic) IBOutlet UITextField *smsCode;
+
+@end
